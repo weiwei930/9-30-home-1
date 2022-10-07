1b14d1ad5554c221dcc1236a52b16ba7df89e8f8
weiwei930 <114204147+weiwei930@users.noreply.github.com>
2022-10-07 11:43:51 +0800

    more 迴圈

---
+++ test.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class test{
+	public static void main (String[] args){
+		int gard = 0;
+		String space = "";
+		
+		while (gard < 10){
+			   System.out.printf("%s", space);	
+			   System.out.printf("%d", grade);	
+			   garde = gard + 1 ;
+			   space = space + " ";`
+		}
+	
+	
+	}
+}	
