ed1b67365ff3cfff724dc9ec98c3044e5ed84437
weiwei930 <114204147+weiwei930@users.noreply.github.com>
2022-10-14 11:38:39 +0800

    1017

---
+++ Test7.java
@@ -0,0 +1,22 @@
+public class Test7 {
+	public static void main (String[] args){
+		int i= 1;
+		
+		while(i < 5){
+			int n = 1;			
+			while (n <= 5-i ) {
+			 ystem.out.print("");
+			 tar++;
+			} 
+			int k = 1;
+			while(k<=2*i-1){
+			 ystem.out.println("");	
+			 ++;
+		
+			}
+			System.out.println("");	
+			i++;	
+		 }		
+		
+	}			
+}  	
