de998634d76825c994ecd2f7b57bc5b95661cadf
weiwei930 <114204147+weiwei930@users.noreply.github.com>
2022-10-20 21:41:05 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,17 +1,44 @@
 import java.util.Scanner;
 
-public class test{
-	public static void main (String[] args){
-		int gard = 0;
-		String space = "";
-		
-		while (gard < 10){
-			   System.out.printf("%s", space);	
-			   System.out.printf("%d", grade);	
-			   garde = gard + 1 ;
-			   space = space + " ";`
-		}
-	
-	
-	}
-}	
+public class Test8 {
+    public static void main(String[] args) {
+        int num1;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("input first integer:");
+        num1 = input.nextInt();
+        if (num1 <= 100 && num1 >= 0) { 
+
+
+                switch (num1/10)
+                {
+                    case 9:
+                    case 10:
+                        System.out.println("A");
+                        break;
+
+                    case 8:
+                        System.out.println("B");
+                        break;
+
+                    case 7:
+                        System.out.println("C");
+                        break;
+
+                    case 6:
+                        System.out.println("D");
+                        break;
+
+                    default:
+                        System.out.println("E");
+                        break;
+                }
+
+        }
+         	else {
+           		 System.out.println("not find!");
+       		 }
+
+
+    }
+}
